215aac61f952951b0a3d98763aa6c92685845c71
And42 <andrewskobcov@yandex.ru>
2019-04-28 13:43:45 +0300

    Added current mapping state display

---
+++ App/Logic/Converters/RecordingStateToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using App.ViewModels;
+using MVVM_Tools.Code.Classes;
+
+namespace App.Logic.Converters
+{
+    public class RecordingStateToStringConverter : ConverterBase<NewMappingWindowViewModel.RecordingStates, string>
+    {
+        public override string ConvertInternal(NewMappingWindowViewModel.RecordingStates value, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case NewMappingWindowViewModel.RecordingStates.Idle:
+                    return "Recording stopped";
+                case NewMappingWindowViewModel.RecordingStates.SourceKey:
+                    return "Recording source key";
+                case NewMappingWindowViewModel.RecordingStates.MappedKey:
+                    return "Recording mapped key";
+                default:
+                    throw new Exception("unknown recording state");
+            }
+        }
+    }
+}
